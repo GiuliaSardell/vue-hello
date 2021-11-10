0c7e7421dccf697b91393b5bd213a942573d72f5
Giulia Sardell <giuliasardelli1234@gmail.com>
2021-11-10 15:20:39 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    testo: 'Ciao',
+    nome: 'Giulia',
+    colore: 'rosa'
+  },
+
+  methods: {
+    saluta(){
+      this.testo="Ciao "+this.nome
+      console.log('ciao ' + this.nome)
+    },
+
+    coloreBlue(){
+      this.colore = "blue"
+    },
+
+    coloreRosa(){
+      this.colore="rosa"
+    },
+
+    cancella(){
+      this.testo=""
+    }
+
+  }
+
+})
